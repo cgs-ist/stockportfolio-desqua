5d9169b082da2415be7891fb627d860608310016
desqua <desqua55@gmail.com>
2023-07-27 10:04:35 +1000

    initial setup

---
+++ src/Stock.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Stock {
+    private String symbol;
+    private String name;
+    private double price;
+
+    public Stock(String symbol, String name, double price) {
+        this.symbol = symbol;
+        this.name = name;
+        this.price = price;
+    }
+
+}
---
+++ src/StockPortfolio.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class StockPortfolio {
+    private HashMap<Stock, Integer> allStocks = new HashMap<Stock, Integer>();
+
+    public StockPortfolio() {
+
+    }
+
+    public void addStock(Stock stock, int quantity) {
+        allStocks.put(stock, quantity);
+    }
+
+    public void viewPortfolio() {
+
+    }
+}
